15dda24dcbaf5a1722f59a0a77dd4a87dd3b92d2
lz1oceani <z6ling@ucsd.edu>
2021-11-22 21:47:59 -0800

    Support SyncBN

--- torchsparse/nn/modules/norm.py
+++ torchsparse/nn/modules/norm.py
@@ -3,14 +3,104 @@ from torch import nn
 
 from torchsparse import SparseTensor
 from torchsparse.nn.utils import fapply
+import torch.nn.functional as F
+from torch.nn.modules.batchnorm import _NormBase
 
-__all__ = ['BatchNorm', 'GroupNorm']
+__all__ = ['BatchNorm', 'GroupNorm', 'SyncBatchNorm']
 
 
-class BatchNorm(nn.BatchNorm1d):
+
+class BatchNorm(_NormBase):
+    def __init__(
+        self,
+        num_features,
+        eps=1e-5,
+        momentum=0.1,
+        affine=True,
+        track_running_stats=True,
+        device=None,
+        dtype=None
+    ):
+        factory_kwargs = {'device': device, 'dtype': dtype}
+        super(BatchNorm, self).__init__(
+            num_features, eps, momentum, affine, track_running_stats, **factory_kwargs
+        )
 
     def forward(self, input: SparseTensor) -> SparseTensor:
+        self._check_input_dim(input)
+
+        if self.momentum is None:
+            exponential_average_factor = 0.0
+        else:
+            exponential_average_factor = self.momentum
+
+        if self.training and self.track_running_stats:
+            # TODO: if statement only here to tell the jit to skip emitting this when it is None
+            if self.num_batches_tracked is not None:  # type: ignore[has-type]
+                self.num_batches_tracked = self.num_batches_tracked + 1  # type: ignore[has-type]
+                if self.momentum is None:  # use cumulative moving average
+                    exponential_average_factor = 1.0 / float(self.num_batches_tracked)
+                else:  # use exponential moving average
+                    exponential_average_factor = self.momentum
+        if self.training:
+            bn_training = True
+        else:
+            bn_training = (self.running_mean is None) and (self.running_var is None)
+
+        return fapply(
+            input,
+            F.batch_norm,
+            # If buffers are not to be tracked, ensure that they won't be updated
+            self.running_mean
+            if not self.training or self.track_running_stats
+            else None,
+            self.running_var if not self.training or self.track_running_stats else None,
+            self.weight,
+            self.bias,
+            bn_training,
+            exponential_average_factor,
+            self.eps,
+        )
+
+    def _check_input_dim(self, input):
+        if input.feats.dim() != 2 and input.feats.dim() != 3:
+            raise ValueError("expected 2D or 3D input (got {}D input)".format(input.feats.dim()))
+
+    
+class SyncBatchNorm(nn.SyncBatchNorm):
+    def forward(self, input: SparseTensor) -> SparseTensor:
         return fapply(input, super().forward)
+
+    @classmethod
+    def convert_sync_batchnorm(cls, module, process_group=None):
+        """
+        Copied from https://pytorch.org/docs/stable/_modules/torch/nn/modules/batchnorm.html#SyncBatchNorm
+        """
+        module_output = module
+        if isinstance(module, BatchNorm):
+            module_output = SyncBatchNorm(
+                module.num_features,
+                module.eps,
+                module.momentum,
+                module.affine,
+                module.track_running_stats,
+                process_group,
+            )
+            if module.affine:
+                with torch.no_grad():
+                    module_output.weight = module.weight
+                    module_output.bias = module.bias
+            module_output.running_mean = module.running_mean
+            module_output.running_var = module.running_var
+            module_output.num_batches_tracked = module.num_batches_tracked
+            if hasattr(module, "qconfig"):
+                module_output.qconfig = module.qconfig
+        for name, child in module.named_children():
+            module_output.add_module(
+                name, cls.convert_sync_batchnorm(child, process_group)
+            )
+        del module
+        return module_output
 
 
 class GroupNorm(nn.GroupNorm):
